9ff67ebf1f82d3c9b6d82c1b1d71a428b77f480c
Jumper Luko <jumper.luko@gmail.com>
2019-05-08 15:27:10 -0300

    Updated css + adjusts, removed comments

--- produto.php
+++ produto.php
@@ -12,8 +12,7 @@ require_once('rdd/head.php');
             }
         }
     </style>
-    <!-- <div class="col-12 menu-cover" style="height:100px;background-color: #011C2E;"></div> -->
-    <header class="col-12 startW" id="menu" style="display: flex;z-index:0;background-color: #011C2E;position: unset;">
+    <header class="col-12 flex" id="menu" style="display: flex;z-index:0;background-color: #011C2E;position: unset;">
     	<a href=".#produtos" class="col-12 wow bounceInLeft center" style="height: 100px;"><span style="font-size: 50px;"><i class="fas fa-angle-double-left"></i>Voltar</span></a>
     </header>
     <?php
@@ -21,8 +20,7 @@ require_once('rdd/head.php');
         $consulta = "select post.name_post,post.imgCapa_post,post.post_post from post where post.name_post='".$_GET['nome']."';";
         $query = mysqli_query($conexao,$consulta);
         while($line  =  mysqli_fetch_array($query)){
-           echo '<div class="center centerText h1 col-12" style="background-image: url(\'img/onda.svg\');background-attachment: fixed;padding:2vw;color:#E2FDF6;font-weight:bold;"><span class="wow tada">'.$line['name_post'].'</span></div>';
-           // echo '<div class="center h3" style="width:100%;">'.$line['imgCapa_post'].'</div>';
+           echo '<div class="center centerText h1 col-12" style="background-image: url(\'img/onda.svg.png\');background-attachment: fixed;padding:2vw;color:#E2FDF6;font-weight:bold;"><span class="wow tada">'.$line['name_post'].'</span></div>';
            echo $line['post_post'];
         }
     }else {
@@ -31,25 +29,14 @@ require_once('rdd/head.php');
     ?>
     <script src="js/zooming.min.js"></script>
     <script>
-        new Zooming({scaleExtra:30,scrollThreshold:100}).listen('img');
+        new Zooming({scaleExtra:10,scrollThreshold:100}).listen('img');
     </script>
 
-    <!-- <script>
-    $("#zoom_mw").elevateZoom({gallery:'galery', galleryActiveClass: 'active-zoom', scrollZoom : true, tint:true, tintColour:'#000', tintOpacity:0.0, cursor: 'crosshair', easing:true,zoomWindowWidth:500, zoomWindowHeight:500,zoomWindowPosition: 1, zoomWindowFadeIn: 500, zoomWindowFadeOut: 500,lensFadeIn: true, lensFadeOut: true, zoomWindowOffetx:1, responsive: true});
-    // $("#zoom_mw").elevateZoom({gallery:'galery', galleryActiveClass: 'active',zoomWindowWidth:0, zoomWindowHeight:0, galleryActiveClass: "active-galery"});
-    //pass the images to Fancybox
-	$("#zoom_mw").bind("click", function(e) {
-	  var ez =   $('#zoom_mw').data('elevateZoom');
-		$.fancybox(ez.getGalleryList());
-	  return false;
-	});
-    </script> -->
-
-    <footer class="col-12 around wrap contato" id="contato" style="overflow: hidden;background: #000000;">
-        <div class="col-12 centerW centerText h1" style="margin:40px 4vw 0 4vw;">Contato</div>
-        <div class="centerW centerText h3" style="margin: 40px 1vw 40px 1vw;"><div class="col-12 center"><span><i class="fa fa-phone"></i></span></div><span>(47) 3026-5141</span></div>
-        <div class="centerW centerText h3" style="margin: 40px 1vw 40px 1vw;"><div class="col-12 center"><span><i class="fas fa-at col-12"></i></span></div><span>comercial.marecia@gmail.com</span></div>
-        <div class="centerW centerText h3" style="margin: 40px 1vw 40px 1vw;"><div class="col-12 center"><span><i class="fas fa-map-marker col-12"></i></span></div><span>Rua Prefeito Helmuth Fallgatter, 1208<br> 89206-100 – Boa Vista – Joinville/SC</span></div>
+    <footer class="col-12 flex around wrap contato" id="contato" style="overflow: hidden;background: #000000;">
+        <div class="col-12 flex center wrap centerText h1" style="margin:40px 4vw 0 4vw;">Contato</div>
+        <div class="flex center wrap centerText h3" style="margin: 40px 1vw 40px 1vw;"><div class="col-12 flex center"><span><i class="fa fa-phone"></i></span></div><span>(47) 3026-5141</span></div>
+        <div class="flex center wrap centerText h3" style="margin: 40px 1vw 40px 1vw;"><div class="col-12 flex center"><span><i class="fas fa-at col-12"></i></span></div><span>comercial.marecia@gmail.com</span></div>
+        <div class="flex center wrap centerText h3" style="margin: 40px 1vw 40px 1vw;"><div class="col-12 flex center"><span><i class="fas fa-map-marker col-12"></i></span></div><span>Rua Prefeito Helmuth Fallgatter, 1208<br> 89206-100 – Boa Vista – Joinville/SC</span></div>
     </footer>
 </body>
 </html>
--- rdd/head.php
+++ rdd/head.php
@@ -14,17 +14,18 @@
     <meta name="rating" content="general" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="manifest.json">
+
+    <link rel="stylesheet" type="text/css" href="css/responsive_flex-grid.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/pages.css">
-    <link rel="stylesheet" type="text/css" href="css/responsive_flex.css">
     <link rel="stylesheet" type="text/css" href="css/animate.min.css">
     <link rel="stylesheet" type="text/css" href="fontawesome-free-5.0.13/css/fontawesome-all.min.css">
     <!--<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans%3A1%2C300%2C400%2C400italic%2C500%2C600%2C700%2C700italic&#038;ver=4.9.3" />-->
 
     <script src="../js/jquery-3.3.1.min.js"></script>
     <script src="js/wow.min.js"></script>
-    <script src='js/jquery-1.8.3.min.js'></script>
-    <script src='js/jquery.elevatezoom.js'></script>
+    <!-- <script src='js/jquery-1.8.3.min.js'></script>
+    <script src='js/jquery.elevatezoom.js'></script> -->
     <script>
         new WOW().init();
     </script>
